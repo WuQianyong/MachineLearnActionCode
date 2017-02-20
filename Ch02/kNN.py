4038c2764d683332035417854d566f17dfb4ca27
VsatYong <wuqianyong@163.com>
2017-02-20 18:04:24 +0800

    part kNN

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -8,9 +8,9 @@
 """
 k-近邻算法
 """
-
-import numpy as np
+from numpy import  *
 import operator
+
 
 class kNN(object):
     def __init__(self):
@@ -21,12 +21,24 @@ class kNN(object):
         创建数据集
         :return:
         """
-        group = np.array([[1.0, 1.1],
+        group = array([[1.0, 1.1],
                           [1, 1],
                           [0, 0],
                           [0, 0.1]])
         labels = ['A', 'A', 'B', 'B']
         return group, labels
+
+    def class_if_y0(self, index, dataset, labels, k):
+        """
+        选择距离最少的 k 个点
+        :param index: 分类的输入向量
+        :param dataset:训练样本集
+        :param labels:标签向量
+        :param k:选择最近邻居的数目
+        :return:
+        """
+        dataset_size = dataset.shape[0]              # shape 是获得 数据的 维度
+        diff_mat = tile()
 
 
 if __name__ == '__main__':
